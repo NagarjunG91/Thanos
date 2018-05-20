e4a12c10479846e50cf3bc3f0baae8f8de9ac433
NagarjunG91 <nagarjunsastry@gmail.com>
2018-05-20 23:45:25 +0530

    checkin

---
+++ bank.py
@@ -0,0 +1,45 @@
+class Bankaccount:
+    def __init__(self):
+        self.balance = 0
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        print('new balance after withdraw {}'.format(self.balance))
+        return self.balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print('new balance after deposit {}'.format(self.balance))
+        return self.balance
+
+
+class MinBalAcc(Bankaccount):
+    def __init__(self, minimum_bal):
+        Bankaccount.__init__(self)
+        self.minimum_bal = minimum_bal
+
+    def withdraw(self, amount):
+        if self.balance - amount < self.minimum_bal:
+            print('please maintain minimum balance of ', self.minimum_bal)
+        else:
+            Bankaccount.withdraw(self, amount)
+
+
+while True:
+
+    a = int(input('please enter minimum balance you want to keep:'))
+    newaccount = MinBalAcc(a)
+
+    newaccount.deposit(a)
+    print('minimum balance deducted')
+
+    b = int(input('Please deposit an amount:'))
+    newaccount.deposit(b)
+
+    c = int(input('Please withdraw an amount:'))
+    newaccount.withdraw(c)
+    d = input('Do you want to Exit (Y/N) :')
+    if d == 'Y':
+        break
+    else:
+        continue
