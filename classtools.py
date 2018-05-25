bea8bf8f83d525e04fea101e7bd72eaf4e8bccb6
NagarjunG91 <nagarjunsastry@gmail.com>
2018-05-25 23:48:04 +0530

    commit

---
+++ classtools.py
@@ -0,0 +1,29 @@
+class AttrDisplay:
+
+    def gatherAttr(self):
+       attrs = []
+       for key in sorted(self.__dict__):
+             attrs.append('%s=%s' % (key, getattr(self, key)))
+       return ','.join(attrs)
+
+    def __repr__(self):
+        return '[%s:%s]' % (self.__class__.__name__, self.gatherAttr())
+
+
+if __name__ == '__main__':
+
+    class TopTest(AttrDisplay):
+        count = 0
+
+        def __init__(self):
+            self.attr1 = TopTest.count
+            self.attr2 = TopTest.count+1
+            TopTest.count += 2
+
+
+    class ST (TopTest):
+        pass
+
+X,Y = (TopTest(), ST())
+print(X)
+print(Y)
