68a9dd08d6b2f7a1f1fdaf6bb8146d52048c3f63
NagarjunG91 <nagarjunsastry@gmail.com>
2018-06-03 22:31:13 +0530

    commit

--- mySQL.py
+++ mySQL.py
@@ -3,12 +3,21 @@ import pymysql
 conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='password', db='mydb')
 cur = conn.cursor()
 cur.execute("SELECT emp_id,emp_name FROM EMPLOYEE")
+print(cur.fetchall())
 
-for row in cur:
-    print(row)
+#for row in cur:
+#   print(row)
+
+#with conn.cursor() as cursor:
+#    sql = "INSERT INTO EMPLOYEE (emp_id, emp_name, emp_salary, emp_add) VALUES (%s, %s, %s, %s)"
+#    cursor.execute(sql, (345, 'Shunya', 99999, 'nowhere'))
+#
+#conn.commit()
 
 
-
+curr = conn.cursor()
+curr.execute("SELECT COUNT(*) FROM EMPLOYEE")
+print(curr.fetchone())
 
 
 
